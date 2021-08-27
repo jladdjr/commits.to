a115c849636ae671510db593dc774a1ecc488a30
Jim Ladd <jladd@redhat.com>
2021-08-27 16:12:45 -0700

    more idonethis -> commits.to

--- commitsto/core/views.py
+++ commitsto/core/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("First view for idonethis. Howdy!")
+    return HttpResponse("First view for commits.to. Howdy!")
